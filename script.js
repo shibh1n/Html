dbfc980748c9a81d400c60d1f7ebf3de28704502
shibh1n <sachushibhin60@gmail.com>
2025-02-18 15:17:13 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function submitData() {
+    //to receive name
+    var name= document.getElementById('name').value
+
+    //to receive dob
+    var dob= document.getElementById('dob').value
+
+    console.log(name + " "+dob)
+}
